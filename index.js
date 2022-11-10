8e3b9143a4ac25f89f1bd63ee9fe97857156b384
Amauri <hamauri@aol.com>
2022-11-10 10:27:21 -0800

    imported modules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +10,9 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
